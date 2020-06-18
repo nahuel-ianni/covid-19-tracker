c321532140b412712701f0d4c40a977d9822079a
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-18 21:34:46 +0200

    Added footer content

--- src/src/components/Footer/Footer.jsx
+++ src/src/components/Footer/Footer.jsx
@@ -8,6 +8,7 @@ const Footer = () => {
             <SocialMedia />
 
             <p>Made by Nahuel Ianni</p>
+            <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">Flaticon</a></p>
         </footer>
     );
 }
