eed8adfcdc4864453825a3e55306ec3ea8900f1e
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-18 21:34:54 +0200

    Changed title

--- src/src/components/Header/Header.jsx
+++ src/src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <h1>COVID 19 - Tracker</h1>
-            <p>Stay safe</p>
+            <h1>COVID-19 Tracker</h1>
+            <p>- Stay safe -</p>
         </header>
     );
 }
