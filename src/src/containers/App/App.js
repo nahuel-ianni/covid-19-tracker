0298e324fae14a6e619a019cac19a79093af61fc
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-22 16:18:08 +0200

    Added header and footer

--- src/src/containers/App/App.js
+++ src/src/containers/App/App.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
 
+import { SocialMediaLinks } from '../../components';
+
+import { Typography } from '@material-ui/core';
+
 class App extends React.Component {
-	render() {
-		return (
-			<Fragment>
-				Navi
-			</Fragment>
-		)
-	}
+    render() {
+        return (
+            <Fragment>
+                <header>
+                    <Typography component="h1" variant="h3">COVID-19 Tracker</Typography>
+                    <Typography component="p" variant="body1">- Stay safe -</Typography>
+                </header>
+
+
+                <footer>
+                    <SocialMediaLinks />
+
+                    <p>Made by Nahuel Ianni</p>
+                    <p>Icons made by <a className="link" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a className="link" href="https://www.flaticon.com/" title="Flaticon">Flaticon</a></p>
+                </footer>
+            </Fragment>
+        )
+    }
 }
 
 export default App;
