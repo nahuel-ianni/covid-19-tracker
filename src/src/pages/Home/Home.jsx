e4034bff3a38ede3f91545915f75834e9457f702
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-22 18:16:22 +0200

    Added protective measure component to the page

--- src/src/pages/Home/Home.jsx
+++ src/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { ProtectiveMeasures } from '../../components'
+
 import { Container } from '@material-ui/core';
 
 export default function Home() {
     return (
         <Container>
-            Navi
+            <ProtectiveMeasures />
         </Container>
     )
 }
