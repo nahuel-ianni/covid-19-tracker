1174b80ea91bd73db3e2ddffcacd5e50e6f4c96e
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-22 15:59:22 +0200

    Added images and index exporting them

---
+++ src/src/images/index.js
@@ -0,0 +1,8 @@
+export const CoveredCoughSvg = require('./img-covered-cough.svg');
+export const FaceMaskSvg = require('./img-face-mask.svg');
+export const HandSanitizerSvg = require('./img-hand-sanitizer.svg');
+export const HelpPhoneSvg = require('./img-help-phone.svg');
+export const NewspaperSvg = require('./img-newspaper.svg');
+export const ProtectedFaceSvg = require('./img-protected-face.svg');
+export const SocialDistancingSvg = require('./img-social-distancing.svg');
+export const WarningMallSvg = require('./img-warning-mall.svg');
