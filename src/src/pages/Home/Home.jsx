b9f862c879cf6be93ea18c63eff41a8ee23f9f77
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-22 17:34:29 +0200

    Added home page component

---
+++ src/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from '@material-ui/core';
+
+export default function Home() {
+    return (
+        <Container>
+            Navi
+        </Container>
+    )
+}
