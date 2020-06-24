abae24a6296ec8d6e8047c87ea336cec88ea194c
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-24 10:52:34 +0200

    Exported card component

--- src/src/components/index.js
+++ src/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as AuthorLinks } from './AuthorLinks/AuthorLinks';
 export { default as PreventiveMeasures } from './PreventiveMeasures/PreventiveMeasures';
+export { default as StatisticCard } from './StatisticCard/StatisticCard';
