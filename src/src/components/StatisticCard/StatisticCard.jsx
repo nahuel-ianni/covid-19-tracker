aeb5e1414479daf616094ee4f3af8f373d6c2dab
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-24 10:52:20 +0200

    Added card with example content

---
+++ src/src/components/StatisticCard/StatisticCard.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+
+import { Card, CardHeader, CardContent, CardActions, Collapse, Avatar, IconButton, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: '30%',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: '#ff726f',
+    },
+}));
+
+const StatisticCard = () => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Title" subheader="Subtitle"
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        !
+                    </Avatar>
+                }
+            />
+
+            <CardContent>
+                <Typography component="p" variant="body2" color="textSecondary">
+                    This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                </Typography>
+            </CardContent>
+
+            <CardActions disableSpacing>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+
+                <IconButton onClick={handleExpandClick}
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}                    
+                    aria-expanded={expanded} aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+
+            <Collapse in={expanded} timeout="auto">
+                <CardContent>
+                    <Typography>Method:</Typography>
+                    <Typography>
+                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.
+                    </Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
+}
+
+export default StatisticCard;
