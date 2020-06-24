85c13c209b16b12b65edda062471122a2b27e357
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-24 18:16:11 +0200

    Deleted old version of the application

--- src-v1/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Footer } from './Footer/Footer';
-export { default as Header } from './Header/Header';
-export { default as ProtectiveMeasures } from './ProtectiveMeasures/ProtectiveMeasures';
-export { default as SocialMedia } from './SocialMedia/SocialMedia';
-export { default as StatsSummary } from './StatsSummary/StatsSummary';
