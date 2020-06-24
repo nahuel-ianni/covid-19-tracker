70b23c4e1a8856c754ef11570ec05ccf94fb283c
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-24 19:37:42 +0200

    Removed data gathering from component

--- src/src/components/StatisticsPanel/StatisticsPanel.jsx
+++ src/src/components/StatisticsPanel/StatisticsPanel.jsx
@@ -1,22 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import styles from './StatisticsPanel.module.css';
 import { StatisticsCard } from '../../components';
-import { GetCardValues } from '../../data/service';
 
 const StatisticsPanel = (props) => {
-    const [listItems, setListItems] = useState([]);
-
-    useEffect(() => {
-        const getData = async () => { setListItems(await GetCardValues()); }
-
-        getData();
-    }, []);
-
     return (
         <ul className={styles.ul}>
             {
-                listItems.map((item, i) =>
+                props.values.map((item, i) =>
                     <li key={i}>
                         <StatisticsCard title={item.title} total={item.total} new={item.new} img={item.img} type={item.type} />
                     </li>
