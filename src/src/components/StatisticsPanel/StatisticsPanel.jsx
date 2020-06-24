2f6a7bc0c5c3db9253aeddf17aa00f9766720312
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-06-24 18:48:31 +0200

    Simplified components

--- src/src/components/StatisticsPanel/StatisticsPanel.jsx
+++ src/src/components/StatisticsPanel/StatisticsPanel.jsx
@@ -4,36 +4,25 @@ import styles from './StatisticsPanel.module.css';
 import { StatisticsCard } from '../../components';
 import { GetCardValues } from '../../data/service';
 
-import { Typography } from '@material-ui/core';
-
 const StatisticsPanel = (props) => {
     const [listItems, setListItems] = useState([]);
 
-    const title = <Typography component="h2" variant="inherit" gutterBottom>Global stats</Typography>;
-    const searchBar = 'IMPLEMENT SEARCH BAR HERE';
-
-    const getHeaderContent = () => props?.restrict === "global" ? title : searchBar;
-
     useEffect(() => {
         const getData = async () => { setListItems(await GetCardValues()); }
-        
+
         getData();
     }, []);
 
     return (
-        <section>
-            {getHeaderContent()}
-
-            <ul className={styles.ul}>
-                {
-                    listItems.map((item, i) =>
-                        <li key={i}>
-                            <StatisticsCard title={item.title} total={item.total} new={item.new} img={item.img} type={item.type} />
-                        </li>
-                    )
-                }
-            </ul>
-        </section>
+        <ul className={styles.ul}>
+            {
+                listItems.map((item, i) =>
+                    <li key={i}>
+                        <StatisticsCard title={item.title} total={item.total} new={item.new} img={item.img} type={item.type} />
+                    </li>
+                )
+            }
+        </ul>
     )
 }
 
--- src/src/pages/Home/Home.jsx
+++ src/src/pages/Home/Home.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
 import { PreventiveMeasures, StatisticsPanel } from '../../components'
-import { RecommendedMeasures } from '../../data/static';
+import { GetRecommendedMeasures } from '../../data/service';
 
 import { Container, Typography } from '@material-ui/core';
 
 const Home = () => {
+    const preventiveMeasures = GetRecommendedMeasures();
+
     return (
         <Container>
-            <StatisticsPanel restrict="global" />
-            <StatisticsPanel />
+            <section>
+                <Typography component="h2" variant="inherit" gutterBottom>Global stats</Typography>
+                <StatisticsPanel />
+            </section>
+
+            {/* <section>
+                ------- ACA VA UNA BARRA DE BUSQUEDA
+                <Typography component="h2" variant="inherit" gutterBottom>PONER EL NOMBRE DEL PAIS, OCULTAR SI NO HAY PAIS SELECCIONADO</Typography>
+                <StatisticsPanel />
+            </section> */}
 
             <section>
-                <Typography component="h2" variant="inherit" gutterBottom>
-                    Preventive measures
-                </Typography>
-
-                <PreventiveMeasures measures={RecommendedMeasures} />
+                <Typography component="h2" variant="inherit" gutterBottom>Preventive measures</Typography>
+                <PreventiveMeasures measures={preventiveMeasures} />
             </section>
         </Container>
     )
