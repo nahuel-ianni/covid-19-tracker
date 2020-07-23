956c5f913ab4850738cdb82e1703e8a383cf623a
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-07-23 17:49:55 +0200

    Simplified image import

--- src/src/data/service/index.js
+++ src/src/data/service/index.js
@@ -1,6 +1,6 @@
 import { getCountries, getCountryPopulation, getLastUpdateDateTime, getSummary } from '../api';
 
-import { CardDeadSvg, CardInfectedSvg, CardRecoveredSvg, CoveredCoughSvg, FaceMaskSvg, HandSanitizerSvg, HelpPhoneSvg, NewspaperSvg, ProtectedFaceSvg, SocialDistancingSvg, WarningMallSvg } from '../../images';
+import * as images from '../../images';
 import { GetPercentage } from '../../utils';
 
 const CardType = {
@@ -32,7 +32,7 @@ const GetDataByCountry = async (countryCode) => {
             title: 'RECOVERED',
             total: data?.TotalRecovered,
             new: data?.NewRecovered,
-            img: CardRecoveredSvg,
+            img: images.CardRecoveredSvg,
             type: CardType.POSITIVE,
             extras: [
                 `${data?.NewRecovered.toLocaleString()} new registered cases`,
@@ -45,7 +45,7 @@ const GetDataByCountry = async (countryCode) => {
             title: 'INFECTED',
             total: data?.TotalConfirmed,
             new: data?.NewConfirmed,
-            img: CardInfectedSvg,
+            img: images.CardInfectedSvg,
             type: CardType.NEUTRAL,
             extras: [
                 `${data?.NewConfirmed.toLocaleString()} new registered cases`,
@@ -58,7 +58,7 @@ const GetDataByCountry = async (countryCode) => {
             title: 'DEATHS',
             total: data?.TotalDeaths,
             new: data?.NewDeaths,
-            img: CardDeadSvg,
+            img: images.CardDeadSvg,
             type: CardType.NEGATIVE,
             extras: [
                 `${data?.NewDeaths.toLocaleString()} new registered cases`,
@@ -90,49 +90,49 @@ const GetRecommendedMeasures = () => [
     {
         title: 'Wash your hands frequently',
         content: 'Regularly and thoroughly clean your hands with an alcohol-based hand rub or wash them with soap and water.',
-        img: HandSanitizerSvg,
+        img: images.HandSanitizerSvg,
         img_desc: 'Wash your hands frequently'
     },
     {
         title: 'Maintain social distance',
         content: 'Maintain at least 1 metre (3 feet) distance between yourself and others.',
-        img: SocialDistancingSvg,
+        img: images.SocialDistancingSvg,
         img_desc: 'Maintain social distance'
     },
     {
         title: 'Stay clear of crowds',
         content: 'Avoid going to crowded places.',
-        img: WarningMallSvg,
+        img: images.WarningMallSvg,
         img_desc: 'Stay clear of crowds'
     },
     {
         title: 'Avoid touching your face',
         content: 'Avoid touching eyes, nose and mouth.',
-        img: ProtectedFaceSvg,
+        img: images.ProtectedFaceSvg,
         img_desc: 'Avoid touching your face'
     },
     {
         title: 'Practice respiratory hygiene',
         content: 'Make sure you, and the people around you, follow good respiratory hygiene. This means covering your mouth and nose with your bent elbow or tissue when you cough or sneeze. Then dispose of the used tissue immediately and wash your hands.',
-        img: CoveredCoughSvg,
+        img: images.CoveredCoughSvg,
         img_desc: 'Practice respiratory hygiene'
     },
     {
         title: 'Behave consciously',
         content: 'Stay home and self-isolate even with minor symptoms such as cough, headache, mild fever, until you recover. Have someone bring you supplies. If you need to leave your house, wear a mask to avoid infecting others.',
-        img: FaceMaskSvg,
+        img: images.FaceMaskSvg,
         img_desc: 'Behave consciously'
     },
     {
         title: 'Seek medical care',
         content: 'If you have a fever, cough and difficulty breathing, seek medical attention, but call by telephone in advance if possible and follow the directions of your local health authority.',
-        img: HelpPhoneSvg,
+        img: images.HelpPhoneSvg,
         img_desc: 'Seek medical care'
     },
     {
         title: 'Stay updated',
         content: 'Keep up to date on the latest information from trusted sources, such as WHO or your local and national health authorities.',
-        img: NewspaperSvg,
+        img: images.NewspaperSvg,
         img_desc: 'Stay updated'
     },
 ];
