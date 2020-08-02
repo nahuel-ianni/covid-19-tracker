3cc26de7e9999cd69c61e4e6fb429db7890199a4
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-08-02 10:20:26 +0200

    Added build

---
+++ precache-manifest.453c97a7be8dca8f0dc62cdc57458869.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccea6fd8b7b680bfec88c87b2e1ecc4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f564523db9fbd84d5444",
+    "url": "./static/css/main.97ba1feb.chunk.css"
+  },
+  {
+    "revision": "78e228348e6aa43710ae",
+    "url": "./static/js/2.fe2488ae.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.fe2488ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f564523db9fbd84d5444",
+    "url": "./static/js/main.621053f2.chunk.js"
+  },
+  {
+    "revision": "6e5f4c5f7db4efb565d4",
+    "url": "./static/js/runtime-main.f89affb8.js"
+  },
+  {
+    "revision": "6bf7935f41f5a687cd0d77b12cffaba9",
+    "url": "./static/media/img-card-dead.6bf7935f.svg"
+  },
+  {
+    "revision": "d422cf44213f0bcded3b6969c4156ecc",
+    "url": "./static/media/img-card-infected.d422cf44.svg"
+  },
+  {
+    "revision": "d5bc9d01555afbf7538df07be639a454",
+    "url": "./static/media/img-card-recovered.d5bc9d01.svg"
+  },
+  {
+    "revision": "a2b91ff67dd7a1e57e58756760935602",
+    "url": "./static/media/img-covered-cough.a2b91ff6.svg"
+  },
+  {
+    "revision": "0559d6615e294433c39d5e4cd629e53a",
+    "url": "./static/media/img-face-mask.0559d661.svg"
+  },
+  {
+    "revision": "2335736b55fb2f075009482ba436bd83",
+    "url": "./static/media/img-hand-sanitizer.2335736b.svg"
+  },
+  {
+    "revision": "571f590feea0e22ab45ea59b399e2f2a",
+    "url": "./static/media/img-help-phone.571f590f.svg"
+  },
+  {
+    "revision": "e3118641f4785f44b195a17e92dd80df",
+    "url": "./static/media/img-newspaper.e3118641.svg"
+  },
+  {
+    "revision": "ab0eb2e25296a0a7a02567e0849d9b38",
+    "url": "./static/media/img-protected-face.ab0eb2e2.svg"
+  },
+  {
+    "revision": "5a6b24de81eee7f4e931780ba6274782",
+    "url": "./static/media/img-red-virus.5a6b24de.svg"
+  },
+  {
+    "revision": "ab46258a50d3b9976fef9954150b8e8d",
+    "url": "./static/media/img-social-distancing.ab46258a.svg"
+  },
+  {
+    "revision": "baae83944b8ffb1ad1d84120026b51ab",
+    "url": "./static/media/img-warning-mall.baae8394.svg"
+  }
+]);
--- src/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as AuthorLinks } from './AuthorLinks/AuthorLinks';
-export { default as CountryPicker } from './CountryPicker/CountryPicker';
-export { default as PreventiveMeasures } from './PreventiveMeasures/PreventiveMeasures';
-export { default as SocialMediaPanel } from './SocialMediaPanel/SocialMediaPanel';
-export { default as StatisticsPanel } from './StatisticsPanel/StatisticsPanel';
-export { default as StatisticsCard } from './StatisticsCard/StatisticsCard';
