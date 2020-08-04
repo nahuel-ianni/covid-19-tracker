3aa8c79770cd8ecbfd13b6a07e656821d6620955
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-08-04 09:17:49 +0200

    Style changes

--- src/src/components/StatisticsMap/StatisticsMap.jsx
+++ src/src/components/StatisticsMap/StatisticsMap.jsx
@@ -8,6 +8,7 @@ const StatisticsMap = () => {
     return (
         <Fragment>
             <h2 className={styles.header}>Coronavirus (COVID-19)</h2>
+            
             <img src={HeaderBackground} alt="Map of Covid-19 cases across the globe" />
         </Fragment>
     );
