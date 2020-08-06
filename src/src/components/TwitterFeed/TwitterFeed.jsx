604e5e397702a8cb6ea99ef433d7699428abb428
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-08-06 15:43:09 +0200

    Added twitter feedback back

--- src/src/components/TwitterFeed/TwitterFeed.jsx
+++ src/src/components/TwitterFeed/TwitterFeed.jsx
@@ -6,7 +6,7 @@ const TwitterFeed = () => {
     return (
         <Card className="card" variant="outlined">
             <CardContent className="cardContent">
-                {/* <Link className="twitter-timeline" data-width="300" data-height="500" href="https://twitter.com/WHO?ref_src=twsrc%5Etfw">Tweets by WHO</Link> */}
+                <Link className="twitter-timeline" data-width="300" data-height="500" href="https://twitter.com/WHO?ref_src=twsrc%5Etfw">Tweets by WHO</Link>
             </CardContent>
         </Card>
     );
