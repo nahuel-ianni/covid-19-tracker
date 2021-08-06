7a51ec5455b6a1eba8ccfcafdd2f21dd1a3935d3
Nahuel Ianni <8545295+nahuel-ianni@users.noreply.github.com>
2021-08-06 10:50:37 +0200

    Added fixed background

--- src/src/components/CasesMap/CasesMap.jsx
+++ src/src/components/CasesMap/CasesMap.jsx
@@ -7,8 +7,10 @@ import { HeaderBackground } from '../../images';
 const CasesMap = () => {
     return (
         <Fragment>
-            <h2 className={styles.header}>Coronavirus (COVID-19)</h2>            
-            <img src={HeaderBackground} className={styles.img} alt="Map of Covid-19 cases across the globe" />
+            <h2 className={styles.header}>Coronavirus (COVID-19)</h2>
+            <div className={styles.imgContainer}>
+                <img src={HeaderBackground} className={styles.img} alt="Map of Covid-19 cases across the globe" />
+            </div>
         </Fragment>
     );
 }
